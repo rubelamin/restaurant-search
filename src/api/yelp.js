f606b55e188eed08aca60b65b7a4044796f68064
Rubel Amin <bagha.vai100@gmail.com>
2022-04-06 11:31:26 +0600

    Bearer fixed

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.yelp.com/v3/businesses",
     headers: {
-        Authorization: "Bearer 2YPBsWrGa5vYqJeE6hIOm9yghTmPCjXKH1A9VB0mLiZrck_utjme7UlfQZlfNwGJbdOtTS2LHyVtNMY4UBqNC7ZrM2lW-m0dcmae4N1HebJrCc6IsKvIBtO6d3hIYnYx"
+        Authorization: "Bearer yelp api here"
     }
 })
